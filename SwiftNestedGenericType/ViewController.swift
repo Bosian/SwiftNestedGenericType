006aaf60d8585ba7224c778d686c775d6318f2f8
Bosian <kikyou77111@gmail.com>
2015-12-30 23:59:25 +0800

    Initial Commit

---
+++ SwiftNestedGenericType/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftNestedGenericType
+//
+//  Created by 劉柏賢 on 2015/12/30.
+//  Copyright © 2015年 劉柏賢. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
